2bbd9364545b423b2c90839a563c487437341535
sv8520 <ver.karcolova@gmail.com>
2021-12-30 20:13:06 +0100

    refdx

--- main.c
+++ main.c
@@ -6,7 +6,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 //neviem zatial co tu ideme robit
+
+
+char login[100];
+int sockfd = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int posliSpravu() {
+    int n;
+    char contact[100];
+    char buffer[256];
+    printf("Please enter contact: ");
+    bzero(contact,100);
+    fgets(contact, 99, stdin);
+    n = write(sockfd, contact, strlen(contact));
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return 5;
+    }
+
+
+    printf("Please enter a message: ");
+
+
+    bzero(buffer,256);
+    fgets(buffer, 255, stdin);
+
+    n = write(sockfd, buffer, strlen(buffer));
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return 5;
+    }
+
+    bzero(buffer,256);
+    n = read(sockfd, buffer, 255);
+    if (n < 0)
+    {
+        perror("Error reading from socket");
+        return 6;
+    }
+
+    printf("%s\n",buffer);
+
+
+    return 0;
+
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -53,7 +103,29 @@ int main(int argc, char *argv[])
 
     //
 
+    printf("Please enter login: ");
+    bzero(login,100);
+    fgets(login, 99, stdin);
+    n = write(sockfd, login, strlen(login));
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return 5;
+    }
+
+    printf("Pre poslanie spravy zadajte - 1");
+    char zadane = getchar();
+    pthread_t klient;
+    pthread_create(&klient, NULL, posliSpravu, NULL);
+
+    if(zadane == '1'){
+        pthread_join(klient,NULL);
+    }
+
+    /*
     printf("Please enter a message: ");
+
+
     bzero(buffer,256);
     fgets(buffer, 255, stdin);
 
@@ -75,6 +147,6 @@ int main(int argc, char *argv[])
     printf("%s\n",buffer);
     close(sockfd);
 
-    return 0;
+    return 0;*/
 
 }
