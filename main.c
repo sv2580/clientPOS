a82765a8aeba9ebe722617ed0cb9343adac33a64
sv8520 <ver.karcolova@gmail.com>
2022-01-01 20:26:01 +0100

    .

--- main.c
+++ main.c
@@ -11,88 +11,81 @@
 char login[100];
 int sockfd = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-void * posliSpravu() {
+
+void *posliSpravu() {
     int skonci = 0;
-    while(skonci == 0 ){
+    while (skonci == 0) {
         int n;
         char contact[100];
         char buffer[256];
-        while(1){
 
-            printf("Please enter contact: ");
-            bzero(contact,100);
-            printf("%s", "> ");
-            fflush(stdout);
-            scanf("%s",contact);
-            n = write(sockfd, contact, strlen(contact));
-            if (n < 0)
-            {
-                perror("Error writing to socket");
-                return NULL;
-            }        if(strcmp(contact, "exit") != 0 ){
-                skonci = 1;
-                break;
-            }
+        printf("Please enter contact: ");
+        bzero(contact, 100);
+        printf("%s", "> ");
+        fflush(stdout);
+        scanf("%s", contact);
+        n = write(sockfd, contact, strlen(contact));
+        if (n < 0) {
+            perror("Error writing to socket");
+            return NULL;
         }
-        while(1){
-            printf("Please enter a message to send to %s: ", contact);
-            printf("%s", "> ");
-            fflush(stdout);
-            scanf("%s",buffer);
+        if (strcmp(contact, "exit") == 0) {
+            skonci = 1;
+            break;
+        }
 
-            n = write(sockfd, buffer, strlen(buffer));
-            if (n < 0)
-            {
-                perror("Error writing to socket");
-                return NULL;
-            }
-            if(strcmp(contact, "exit") != 0 ){
-                skonci = 1;
-                break;
-            }
+        printf("Please enter a message to send to %s: ", contact);
+        printf("%s", "> ");
+        fflush(stdout);
+        scanf("%s", buffer);
+
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0) {
+            perror("Error writing to socket");
+            return NULL;
         }
+        if (strcmp(contact, "exit") == 0) {
+            skonci = 1;
+            break;
+        }
+
     }
 }
 
-int main(int argc, char *argv[])
-{
-    int  n;
+int main(int argc, char *argv[]) {
+    int n;
     struct sockaddr_in serv_addr;
-    struct hostent* server;
+    struct hostent *server;
 
     char buffer[256];
 
-    if (argc < 3)
-    {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
         return 1;
     }
 
     server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
+    if (server == NULL) {
         fprintf(stderr, "Error, no such host\n");
         return 2;
     }
 
-    bzero((char*)&serv_addr, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy(
-            (char*)server->h_addr,
-            (char*)&serv_addr.sin_addr.s_addr,
+            (char *) server->h_addr,
+            (char *) &serv_addr.sin_addr.s_addr,
             server->h_length
     );
     serv_addr.sin_port = htons(atoi(argv[2]));
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
+    if (sockfd < 0) {
         perror("Error creating socket");
         return 3;
     }
 
-    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("Error connecting to socket");
         return 4;
     }
@@ -100,26 +93,25 @@ int main(int argc, char *argv[])
     //
 
     printf("Please enter login: ");
-    bzero(login,100);
+    bzero(login, 100);
     fgets(login, 99, stdin);
     n = write(sockfd, login, strlen(login));
-    if (n < 0)
-    {
+    if (n < 0) {
         perror("Error writing to socket");
         return 5;
     }
     pthread_t klient;
 
     pthread_create(&klient, NULL, posliSpravu, NULL);
-    pthread_join(klient,NULL);
+    pthread_join(klient, NULL);
 
 
-   /* printf("Pre poslanie spravy zadajte - 1");
-    char zadane = getchar();
+    /* printf("Pre poslanie spravy zadajte - 1");
+     char zadane = getchar();
 
-    if(zadane == '1'){
-        pthread_join(klient,NULL);
-    }*/
+     if(zadane == '1'){
+         pthread_join(klient,NULL);
+     }*/
 
 
 
